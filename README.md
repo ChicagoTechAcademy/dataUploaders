6d8a0ae66ed5661eb14d8321c90ef53b2e763d4c
Nick Gutierrez <ngutierrez@chicagotechacademy.org>
2023-09-15 16:34:45 -0500

    schedule uploader

--- README.md
+++ README.md
@@ -8,15 +8,13 @@ Completed:
 - Clean and Upload Weekly Average Daily Attendance
 - Clean and Upload HighFive Data
 - Clean Daily Attendance Totals
-
-
+- Clean and Upload FOCUS Tardy
+- Clean and Upload FOCUS Hall Sweeps
+- Create spreadsheets for FOCUS Tardy and Hall Sweeps
+- Create spreadsheet for Enrollment
 
 ToDo:
 - Create spreadsheets for Total and Weekly Average Daily Attendance
-- Create spreadsheets for FOCUS Tardy and Hall Sweeps
-- Create spreadsheet for Enrollment
-- Clean and Upload FOCUS Tardy
-- Clean and Upload FOCUS Hall Sweeps
 - Upload Classes Offered
 - Upload Grades
 - Clean and Upload Data Quality
--- scripts/schedules_update.py
+++ scripts/schedules_update.py
@@ -1,17 +1,16 @@
+import os
 import pandas as pd
 import pandas_gbq
 from google.cloud import bigquery
+from datetime import datetime
 
-# Define your GCP project ID and BigQuery dataset ID
+# Constants
 project_id = "chitechdb"
-dataset_id = "academics"
 table_id = "academics.schedules"
+source_folder = "../dataUploaders/schedules"
+destination_folder = "../dataUploaders/archivedFiles"
+archive_name = "schedules"
 
-# Initialize the BigQuery client
-client = bigquery.Client(project=project_id)
-print(client)
-
-# Define the column name mappings
 column_mappings = {
     "LastName": "lastName",
     "FirstName": "firstName",
@@ -24,61 +23,58 @@ column_mappings = {
     "Schedule": "period"
 }
 
-# Read the CSV file
-file_path = "schedule.csv"
-df = pd.read_csv(file_path)
+def clean_data(df):
+    """
+    Processes the dataframe for cleanup tasks.
+    """
 
-# Remove columns from the DataFrame
-columns_to_remove = ["MiddleName", "SpecialEdStus", "Homeroom", "Inclusion?", "SecType", "Total", "Max"]
-df = df.drop(columns=columns_to_remove)
+    print("Cleaning data...")
 
-# Rename the columns that need renaming
-df.rename(columns=column_mappings, inplace=True)
+    # Drop unnecessary columns
+    df.drop(columns=["MiddleName", "SpecialEdStus", "Homeroom", "Inclusion?", "SecType", "Total", "Max"], inplace=True, errors='ignore')
+    print("Dropped unnecessary columns.")
 
-# Calculate the "sy" column based on the year and month
-def get_yog(grade):
-    if grade == 9:
-        return 2027
-    elif grade == 10:
-        return 2026
-    elif grade == 11:
-        return 2025
-    elif grade == 12:
-        return 2024
-    return str(grade)  # Convert year to string
+    # Rename columns based on predefined mappings
+    df.rename(columns=column_mappings, inplace=True, errors='raise')
+    print(f"Renamed columns to: {', '.join(column_mappings.values())}")
 
-# Add the "yog" column if it doesn't exist
-if "yog" not in df.columns:
-    df["yog"] = df["grade"].apply(get_yog)
+    # Add 'scheduleAsOf', 'sy', and 'semester' columns
+    df['scheduleAsOf'] = datetime.now().strftime('%Y-%m-%d')
+    df['sy'] = 'SY24'
+    df['semester'] = 'S1'
 
-# Add the "sy" and "semester" columns
-df["sy"] = "SY24"
-df["semester"] = "S1"
+    return df
 
-# Save the DataFrame to a temporary CSV file
-temp_csv_path = "temp_schedule.csv"
-df.to_csv(temp_csv_path, index=False)
 
-# Construct the SQL query to delete old data
-query = f"""
--- Delete old data
-DELETE FROM `{project_id}.{table_id}`
-WHERE 1=1;
-"""
+def delete_old_data_from_db(client):
+    """
+    Deletes old data from the database table.
+    """
+    query = f"DELETE FROM `{project_id}.{table_id}` WHERE 1=1;"
+    query_job = client.query(query)
+    query_job.result()
+    print("Data deletion completed.")
 
-# Execute the delete query
-query_job = client.query(query)
-query_job.result()
 
-print("Data deletion completed.")
+def convert_to_standard_date(date_str):
+    """
+    Converts date from MM/DD/YY format to YYYY-MM-DD.
+    Assumes all two-digit years from 00-21 refer to 2000-2021, and 22-99 refer to 1922-1999.
+    """
+    month, day, year = map(int, date_str.split("/"))
+    if 0 <= year <= 21:
+        year += 2000
+    elif 22 <= year <= 99:
+        year += 1900
+    return f"{year:04d}-{month:02d}-{day:02d}"
 
-# Upload the DataFrame to BigQuery from the temporary CSV file
-pandas_gbq.to_gbq(
-    pd.read_csv(temp_csv_path),
-    destination_table=table_id,
-    project_id=project_id,
-    if_exists="append",
-    table_schema=[
+
+
+def upload_to_big_query(df):
+    """
+    Uploads processed dataframe to BigQuery.
+    """
+    schema = [
         {"name": "id", "type": "INTEGER"},
         {"name": "lastName", "type": "STRING"},
         {"name": "firstName", "type": "STRING"},
@@ -89,10 +85,52 @@ pandas_gbq.to_gbq(
         {"name": "teacher", "type": "STRING"},
         {"name": "period", "type": "STRING"},
         {"name": "yog", "type": "INTEGER"},
+        {"name": "scheduleAsOf", "type": "DATE"},
         {"name": "sy", "type": "STRING"},
         {"name": "semester", "type": "STRING"},
-    ],
-    progress_bar=True,
-)
+    ]
 
-print("Data uploaded to BigQuery table.")
+    pandas_gbq.to_gbq(df, destination_table=table_id, project_id=project_id,
+                      if_exists="append", table_schema=schema, progress_bar=True)
+
+
+def archive_source_file(csv_file):
+    """
+    Moves source CSV to an archive folder with a timestamp.
+    """
+    current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    new_file_name = f"{archive_name}-{current_datetime}.csv"
+    destination_file_path = os.path.join(destination_folder, new_file_name)
+    df.to_csv(destination_file_path, index=False)
+    os.remove(os.path.join(source_folder, csv_file))
+    print(f"File '{csv_file}' has been archived as '{new_file_name}'.")
+
+
+if __name__ == "__main__":
+    print("Starting script...")
+
+    csv_files = [f for f in os.listdir(source_folder) if f.endswith('.csv')]
+    
+    if csv_files:
+        csv_file = csv_files[0]
+        csv_path = os.path.join(source_folder, csv_file)
+
+        df = pd.read_csv(csv_path).dropna(how="all")
+        print("Deleted empty rows from the DataFrame.")
+        
+        client = bigquery.Client(project=project_id)
+        if client:
+            df = clean_data(df)
+            delete_old_data_from_db(client)
+            upload_to_big_query(df)
+            archive_source_file(csv_file)
+        else:
+            print("Failed to initialize BigQuery client.")
+    else:
+        print(f"No CSV files found in the '{archive_name}' folder.")
+
+
+    
+
+# needs to get YOG and upload date for "asOf" and include the school year and semester
+# 

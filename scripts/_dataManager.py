985c3eea23cd312a15660361e2cd5ced7da6924d
Nick Gutierrez <ngutierrez@chicagotechacademy.org>
2024-03-18 15:34:52 -0500

    refactor test and datamanager modules ready

---
+++ scripts/_dataManager.py
@@ -0,0 +1,100 @@
+from datetime import datetime
+import pandas as pd
+import pandas_gbq
+import pydata_google_auth
+import os
+from colorama import init, Fore
+
+init(autoreset=True)
+
+
+SCOPES = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/drive",
+]
+
+credentials = pydata_google_auth.get_user_credentials(
+    SCOPES,
+    # Note, this doesn't work if you're running from a notebook on a
+    # remote sever, such as over SSH or with Google Colab. In those cases,
+    # install the gcloud command line interface and authenticate with the
+    # `gcloud auth application-default login` command and the `--no-browser`
+    # option.
+    auth_local_webserver=True,
+)
+
+def fetchDataFromBigQuery(query):
+    """
+    Fetches data from BigQuery and returns a DataFrame.
+    """
+    print(Fore.BLUE + "Fetching data from BigQuery...")
+    df = pandas_gbq.read_gbq(
+        query,
+        project_id="chitechdb",
+        credentials=credentials,
+    )
+    return df
+
+
+def deleteAllDataFromTable(project_id, dataset_id, table_id):
+    """
+    Deletes all data from the specified BigQuery table.
+    """
+    print(Fore.BLUE + "Deleting old data from database to override...")
+    pandas_gbq.context.credentials = credentials
+    pandas_gbq.context.project = project_id
+    query = f"DELETE FROM `{dataset_id}.{table_id}` WHERE TRUE"
+    pandas_gbq.read_gbq(query)
+    print(Fore.BLUE + f"All data from {table_id} has been deleted.")
+
+
+def uploadToBigQuery(df, schema, project_id, dataset_id, table_id):
+    """
+    Uploads a pandas DataFrame to a BigQuery table.
+    """
+
+    print(Fore.BLUE + "Uploading data to BigQuery...")
+    # Upload the dataframe
+    pandas_gbq.to_gbq(
+        df,
+        destination_table=dataset_id + "." + table_id,
+        project_id=project_id,
+        if_exists="append",
+        progress_bar=True,
+        table_schema=schema,
+    )
+    print(Fore.BLUE + "Data has been uploaded to BigQuery.")
+
+
+def archiveSourceFile(df, sourceFile, sourceFolder, archiveFileName):
+    """
+    Moves the source file to an archive directory.
+    """
+    destination_folder = "../dataUploaders/archivedFiles"
+
+    current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    new_file_name = f"{archiveFileName}-{current_datetime}.csv"
+    destination_file_path = os.path.join(destination_folder, new_file_name)
+    df.to_csv(destination_file_path, index=False)
+    os.remove(os.path.join(sourceFolder, sourceFile))
+    print(Fore.GREEN + f"File '{sourceFile}' has been archived as '{new_file_name}'.")
+
+
+def readCSV(source_folder):
+    """
+    Reads a CSV file and returns the filename found and DataFrame from the read information
+    """
+    print(Fore.GREEN + "Opening the CSV file...")
+
+    csv_files = [f for f in os.listdir(source_folder) if f.endswith(".csv")]
+
+    if csv_files:
+        csv_file = csv_files[0]
+        csv_path = os.path.join(source_folder, csv_file)
+
+        df = pd.read_csv(csv_path).dropna(how="all")
+        print(
+            Fore.GREEN
+            + f"File '{csv_file}' has been read. Empty rows have been removed."
+        )
+        return csv_file, df
---
+++ scripts/_refactorTest.py
@@ -0,0 +1,65 @@
+from _dataManager import *
+
+
+scriptName = "refactorTest"
+
+
+# Constants
+project_id = "chitechdb"
+dataset_id = "test"
+table_id = "testData"
+source_folder = f"../dataUploaders/{table_id}"
+
+tableSchema = [
+    {"name": "name", "type": "STRING"},
+    {"name": "id", "type": "INTEGER"},
+    {"name": "yog", "type": "INTEGER"},
+    {"name": "balance", "type": "FLOAT"},
+]
+
+
+def cleanData(df):
+    """
+    Cleans the data read in from the CSV file and returns a DataFrame.
+    """
+    print(Fore.CYAN + "Cleaning data...")
+
+    # if the balance column has ( ) around it, then convert to a negative number
+    df["balance"] = df["balance"].apply(lambda x: x.replace("(", "-").replace(")", "") if "(" in x else x)
+
+    # convert the balance column to float
+    df["balance"] = df["balance"].astype(float)
+
+    print(Fore.CYAN + "Data has been cleaned.")
+    return df
+
+
+def doWork():
+    """
+    Main function for the script.
+    """
+    print(Fore.YELLOW + f"Starting {scriptName} script...")
+
+    # Read in the first .csv file found
+    csv_file, rawDataFrame = readCSV(source_folder)
+
+    # Clean the data
+    cleanedDataFrame = cleanData(rawDataFrame)
+
+    # Fetch the roster data
+    rosterQuery = f"SELECT * FROM `student_info.roster`"
+    roster_df = fetchDataFromBigQuery(rosterQuery)
+
+    deleteAllDataFromTable(project_id, dataset_id, table_id)
+
+    # Upload the data to BigQuery
+    uploadToBigQuery(cleanedDataFrame, tableSchema, project_id, dataset_id, table_id)
+
+    # Archive the source file
+    archiveSourceFile(cleanedDataFrame, csv_file, source_folder, table_id)
+
+
+    print(Fore.GREEN + f"{scriptName} data pull complete.")
+
+
+doWork()
